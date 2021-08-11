430fd1c64fa8cb0c3b80ab7e86bae3666d399e37
Sebastian <sebastianvr92@hotmail.com>
2021-08-11 15:20:54 -0500

    Added code many_load.py #1

--- scripts/many_load.py
+++ scripts/many_load.py
@@ -1,34 +1,73 @@
+# python3 manage.py runscript many_load
+
+from unesco.models import Category, States, Region, Iso, Site
+
 import csv  # https://docs.python.org/3/library/csv.html
 
 # https://django-extensions.readthedocs.io/en/latest/runscript.html
 
 # python3 manage.py runscript many_load
 
-from many.models import Person, Course, Membership
-
 
 def run():
-    fhand = open("many/load.csv")
+    fhand = open("unesco/whc-sites-2018-clean.csv")
     reader = csv.reader(fhand)
     next(reader)  # Advance past the header
 
-    Person.objects.all().delete()
-    Course.objects.all().delete()
-    Membership.objects.all().delete()
+    Category.objects.all().delete()
+    States.objects.all().delete()
+    Region.objects.all().delete()
+    Iso.objects.all().delete()
+    Site.objects.all().delete()
 
     # Format
-    # email,role,course
-    # jane@tsugi.org,I,Python
-    # ed@tsugi.org,L,Python
+    # name	description	justification	year	longitude	latitude	area_hectares	category	states	region	iso
 
     for row in reader:
-        print(row)
+        # print(row)
 
-        p, created = Person.objects.get_or_create(email=row[0])
-        c, created = Course.objects.get_or_create(title=row[2])
+        category, created = Category.objects.get_or_create(name=row[7])
+        states, created = States.objects.get_or_create(name=row[8])
+        region, created = Region.objects.get_or_create(name=row[9])
+        iso, created = Iso.objects.get_or_create(name=row[10])
 
-        r = Membership.LEARNER
-        if row[1] == "I":
-            r = Membership.INSTRUCTOR
-        m = Membership(role=r, person=p, course=c)
-        m.save()
+        name = row[0]
+        description = row[1]
+        justification = row[2]
+
+        try:
+            year = int(row[3])
+        except:
+            year = None
+
+        try:
+            longitude = float(row[4])
+        except:
+            longitude = None
+
+        try:
+            latitude = float(row[5])
+        except:
+            latitude = None
+
+        try:
+            area_hectares = float(row[6])
+        except:
+            area_hectares = None
+
+        site = Site.objects.create(
+            name=name,
+            description=description,
+            justification=justification,
+            year=year,
+            longitude=longitude,
+            latitude=latitude,
+            area_hectares=area_hectares,
+            category=category,
+            states=states,
+            region=region,
+            iso=iso,
+        )
+        site.save()
+
+    print("Finished")
